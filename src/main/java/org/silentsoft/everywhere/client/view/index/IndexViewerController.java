4ea9330b93972400e7405639a16862b8841adb4a
silentsoft <hs830.lee@gmail.com>
2016-04-16 17:55:16 +0900

    added two kinda viewer that IndexViewer and WikiViewer.

---
+++ src/main/java/org/silentsoft/everywhere/client/view/index/IndexViewerController.java
@@ -0,0 +1,14 @@
+package org.silentsoft.everywhere.client.view.index;
+
+import javafx.scene.Parent;
+
+import org.silentsoft.ui.viewer.AbstractViewerController;
+
+public class IndexViewerController extends AbstractViewerController {
+
+	@Override
+	protected void initialize(Parent viewer, Object... parameters) {
+		
+	}
+
+}
