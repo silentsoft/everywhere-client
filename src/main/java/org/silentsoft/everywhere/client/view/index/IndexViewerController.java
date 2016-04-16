c80dba15a1271f36b645364c93d36e1b1f9620a3
silentsoft <hs830.lee@gmail.com>
2016-04-17 02:06:54 +0900

    modified to open the IndexViewer after login.

--- src/main/java/org/silentsoft/everywhere/client/view/index/IndexViewerController.java
+++ src/main/java/org/silentsoft/everywhere/client/view/index/IndexViewerController.java
@@ -1,7 +1,11 @@
 package org.silentsoft.everywhere.client.view.index;
 
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
 
+import org.silentsoft.everywhere.client.view.login.LoginViewerController;
+import org.silentsoft.everywhere.context.BizConst;
+import org.silentsoft.io.event.EventHandler;
 import org.silentsoft.ui.viewer.AbstractViewerController;
 
 public class IndexViewerController extends AbstractViewerController {
@@ -11,4 +15,13 @@ public class IndexViewerController extends AbstractViewerController {
 		
 	}
 
+	@FXML
+	private void cloud_OnActionClick() {
+		EventHandler.callEvent(LoginViewerController.class, BizConst.EVENT_VIEW_CLOUD);
+	}
+	
+	@FXML
+	private void wiki_OnActionClick() {
+		EventHandler.callEvent(LoginViewerController.class, BizConst.EVENT_VIEW_WIKI);
+	}
 }
